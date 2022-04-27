566e7efb7ae454be31ab18da2cb95013c795d7b6
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-04-27 08:48:29 -0700

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -1,109 +1,67 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | gautamajay52 | MaxxRider
-
 import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 import math
 import os
 import time
 
-from pyrogram.errors.exceptions import FloodWait
-from tobrot import (
-    EDIT_SLEEP_TIME_OUT,
+from apdbot import (
     FINISHED_PROGRESS_STR,
-    UN_FINISHED_PROGRESS_STR,
-    gDict,
-    LOGGER,
+    UN_FINISHED_PROGRESS_STR
 )
-from pyrogram import Client
-
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
-
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    start
+):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        # if round(current / total * 100, 0) % 5 == 0:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
 
-class Progress:
-    def __init__(self, from_user, client, mess: Message):
-        self._from_user = from_user
-        self._client = client
-        self._mess = mess
-        self._cancelled = False
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-    @property
-    def is_cancelled(self):
-        chat_id = self._mess.chat.id
-        mes_id = self._mess.message_id
-        if gDict[chat_id] and mes_id in gDict[chat_id]:
-            self._cancelled = True
-        return self._cancelled
+        progress = "[{0}{1}] \nP: {2}%\n".format(
+            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
+            ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
 
-    async def progress_for_pyrogram(self, current, total, ud_type, start):
-        chat_id = self._mess.chat.id
-        mes_id = self._mess.message_id
-        from_user = self._from_user
-        now = time.time()
-        diff = now - start
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Cancel 🚫",
-                        callback_data=(
-                            f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
-                        ).encode("UTF-8"),
-                    )
-                ]
-            ]
+        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
         )
-        if self.is_cancelled:
-            LOGGER.info("stopping ")
-            await self._mess.edit(
-                f"😔 Cancelled/ERROR: `{ud_type}` ({humanbytes(total)})"
-            )
-            await self._client.stop_transmission()
-
-        if round(diff % float(EDIT_SLEEP_TIME_OUT)) == 0 or current == total:
-            # if round(current / total * 100, 0) % 5 == 0:
-            percentage = current * 100 / total
-            speed = current / diff
-            elapsed_time = round(diff) * 1000
-            time_to_completion = round((total - current) / speed) * 1000
-            estimated_total_time = time_to_completion
-
-            elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-            estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-
-            progress = "\n<code>{0}{1} {2}%</code>\n".format(
-                ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
-                ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
-                round(percentage, 2))
-            #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n**• Total 📀:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed 🚀 :** `〘{2}〙`\n**• ETA ⏳ :**` 〘{3}〙`".format(
-                humanbytes(current),
-                humanbytes(total),
-                humanbytes(speed),
-                # elapsed_time if elapsed_time != '' else "0 s",
-                estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
-            )
-            try:
-                if not self._mess.photo:
-                    await self._mess.edit_text(
-                        text="{}\n {}".format(ud_type, tmp), reply_markup=reply_markup
+        try:
+            if not message.photo:
+                await message.edit_text(
+                    text="{}\n {}".format(
+                        ud_type,
+                        tmp
                     )
-                else:
-                    await self._mess.edit_caption(
-                        caption="{}\n {}".format(ud_type, tmp)
+                )
+            else:
+                await message.edit_caption(
+                    caption="{}\n {}".format(
+                        ud_type,
+                        tmp
                     )
-            except FloodWait as fd:
-                logger.warning(f"{fd}")
-                time.sleep(fd.x)
-            except Exception as ou:
-                logger.info(ou)
+                )
+        except:
+            pass
 
 
 def humanbytes(size):
@@ -111,13 +69,32 @@ def humanbytes(size):
     # 2**10 = 1024
     if not size:
         return ""
-    power = 2 ** 10
+    power = 2**10
     n = 0
-    Dic_powerN = {0: " ", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
     while size > power:
         size /= power
         n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + "B"
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatterr(seconds: int) -> str:
+    result = ""
+    v_m = 0
+    remainder = seconds
+    r_ange_s = {
+        "days": (24 * 60 * 60),
+        "hours": (60 * 60),
+        "minutes": 60,
+        "seconds": 1
+    }
+    for age in r_ange_s:
+        divisor = r_ange_s[age]
+        v_m, remainder = divmod(remainder, divisor)
+        v_m = int(v_m)
+        if v_m != 0:
+            result += f" {v_m} {age} "
+    return result
 
 
 def TimeFormatter(milliseconds: int) -> str:
@@ -125,11 +102,10 @@ def TimeFormatter(milliseconds: int) -> str:
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = (
-        ((str(days) + "d, ") if days else "")
-        + ((str(hours) + "h, ") if hours else "")
-        + ((str(minutes) + "m, ") if minutes else "")
-        + ((str(seconds) + "s, ") if seconds else "")
-        + ((str(milliseconds) + "ms, ") if milliseconds else "")
-    )
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2]
+ 
